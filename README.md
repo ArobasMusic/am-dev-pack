c94113829170f75805a5b708ae821723e617eb11
NealRame <contact@nealrame.com>
2017-12-13 08:54:59 +0100

    Install editor + README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # am-dev-pack
+
 Install all you need to work
+
+## macOS
+
+Just execute the following line in your :
+
+```sh
+❯ curl https://raw.githubusercontent.com/ArobasMusic/am-dev-pack/master/macOS-setup.sh | bash -i
+```
--- macOS-setup.sh
+++ macOS-setup.sh
@@ -17,3 +17,44 @@ brew install python3
 
 # Conan installation
 pip3 install conan conan_package_tools --upgrade
+
+# Install editor
+TMPDIR=$(mktemp -d)
+EDITOR_ZIP="$TMPDIR/editor.zip"
+CODE="https://go.microsoft.com/fwlink/?LinkID=620882"
+ATOM="https://atom.io/download/mac"
+
+while true;
+do
+    echo "Which editor do you want to install?"
+    echo "1. Atom"
+    echo "2. VS Code"
+    echo "3. neither"
+    read -p "> "
+    case $REPLY in
+        1)
+            EDITOR="$ATOM"; break
+            ;;
+        2)
+            EDITOR="$CODE"; break
+            ;;
+        3)
+            EDITOR=none; break
+            ;;
+    esac
+done
+
+if [ $EDITOR != none ];
+then    
+    curl -o "$EDITOR_ZIP" -L "$EDITOR"
+    if groups | grep -q admin;
+    then
+        pushd "/Applications"
+    else
+        pushd "$HOME/Applications"
+    fi
+    unzip -x "$EDITOR_ZIP"
+    popd
+fi
+
+rm -fr "$TMPDIR"
